421e5871c96cdda49186f59c9e465bbd99868984
Geovanny Fajardo <gfajardo@smartprocure.us>
2019-09-11 21:04:10 -0400

    cleanup

--- lib/train.d.ts
+++
@@ -1,9 +0,0 @@
-import brain from 'brain.js';
-export declare let train: ({ brainType, prepocessor, retrain, name, options, trainingOptions }: {
-    brainType: any;
-    prepocessor?: ((x: any) => any) | undefined;
-    retrain: any;
-    name: any;
-    options?: {} | undefined;
-    trainingOptions?: {} | undefined;
-}) => brain.INeuralNetworkJSON;
