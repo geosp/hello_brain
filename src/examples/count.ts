b50a2c214a53006f46ec2726bf0c1c85a00a2d61
Geovanny Fajardo <gfajardo@smartprocure.us>
2019-09-14 14:45:16 -0400

    Added data generation method to count.

--- src/examples/count.ts
+++ src/examples/count.ts
@@ -1,18 +1,33 @@
 import brain from 'brain.js'
 import { train } from '../core/train'
+import _ from 'lodash'
 
 export let count = () => {
   let neuroCount = new brain.recurrent.LSTMTimeStep()
+  let extendDataSet = () => {
+    let a = _.tail([...Array(6).keys()])
+    let b = _.clone(a).reverse()
+    return [
+      a,
+      b,
+    ]
+  }
   neuroCount.fromJSON(
     train({
       brainType: brain.recurrent.LSTMTimeStep,
       name: 'count',
       retrain: false,
       options: { hiddenLayers: [3] },
-      trainingSets: ['forward', 'backward']
+      trainingSets: ['forward', 'backward'],
+      // preprocessor: data => [...data, ...extendDataSet()],
+      // preprocessor: extendDataSet,
+      // trainingOptions: {errorThresh: 0.00005}
     })
   )
-  let countInput = [3,4]
-  // @ts-ignore
-  console.log({ countInput, countOutput: Math.round((neuroCount.run(countInput) as Number))})
+  let countInput = [3, 2]
+  console.log({
+    countInput,
+    // @ts-ignore
+    countOutput: Math.round(neuroCount.run(countInput)),
+  })
 }
