407e666d9319b7f4fe96a2163e2a1569d1e5093f
Geovanny Fajardo <gfajardo@smartprocure.us>
2019-09-25 10:47:28 -0400

    cleanup

--- src/snippets/meanSquaredError.ts
+++ src/snippets/meanSquaredError.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash/fp'
 import F from 'futil-js'
 
-export let meanSquaredError = ({ predicted, expected }) =>
+export let meanSquaredError = ({ predicted = [] as number[], expected = [] as number[] }) =>
   _.flow(
     F.reduceIndexed(
       (sum, expectedValue, i) =>
